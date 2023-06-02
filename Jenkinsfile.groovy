dcfb0eb7a0d58c6154b58eeaebd75708da807c1b
kiran1616 <kirankumar7163@gmail.com>
2023-06-02 12:28:35 +0530

    m

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,29 @@
+pipeline {
+
+  agent {
+    label 'JenkinsAgent'
+  }
+  parameters {
+    string(name: 'component', defaultValue: '', description: 'Enter component')
+    string(name: 'app_version', defaultValue: '', description: 'app_version')
+
+  }
+  stages {
+
+    stage ('get values file') {
+      steps {
+        dir('APP') {
+          git branch: 'main', url: 'https://github.com/kiran6055/${component}.git'
+        }
+      }
+
+    }
+    stage ('Helms file') {
+      steps {
+        sh 'find .'
+      }
+    }
+
+  }
+
+}
