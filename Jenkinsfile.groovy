743ac217d22e85f0579a4d5f6d087859a8739fb3
kiran1616 <kirankumar7163@gmail.com>
2023-06-02 15:06:10 +0530

    m

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -10,15 +10,18 @@ pipeline {
   }
   stages {
 
-    stage ('get values file') {
+    stage ('Get APP File') {
       steps {
         dir('APP') {
           git branch: 'main', url: 'https://github.com/kiran6055/${component}.git'
         }
+        dir('HELM') {
+          git branch: 'main', url: 'https://github.com/kiran6055/roboshop-helm-charts'
+        }
       }
 
     }
-    stage ('Helms file') {
+    stage ('Helms Deploy') {
       steps {
         sh 'helm upgrade -i ${component} . -f APP/values.yaml --set-string image.tag=${app_version}'
       }
